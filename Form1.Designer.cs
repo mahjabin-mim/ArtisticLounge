95f5e2cf9a78e1d7dbc421590a62d3017c81073c
Abu Jubair <abujubair138@gmail.com>
2021-11-18 21:26:17 +0600

    Landing page modified

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,7 +29,135 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ServiceBtn = new System.Windows.Forms.Button();
+            this.AboutusBtn = new System.Windows.Forms.Button();
+            this.ContactBtn = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // ServiceBtn
+            // 
+            this.ServiceBtn.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ServiceBtn.Dock = System.Windows.Forms.DockStyle.Left;
+            this.ServiceBtn.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ServiceBtn.FlatAppearance.BorderSize = 0;
+            this.ServiceBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ServiceBtn.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ServiceBtn.ForeColor = System.Drawing.Color.White;
+            this.ServiceBtn.Location = new System.Drawing.Point(246, 0);
+            this.ServiceBtn.Name = "ServiceBtn";
+            this.ServiceBtn.Size = new System.Drawing.Size(123, 67);
+            this.ServiceBtn.TabIndex = 0;
+            this.ServiceBtn.Text = "Services";
+            this.ServiceBtn.UseVisualStyleBackColor = false;
+            this.ServiceBtn.Click += new System.EventHandler(this.ServiceBtn_Click);
+            // 
+            // AboutusBtn
+            // 
+            this.AboutusBtn.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.AboutusBtn.Dock = System.Windows.Forms.DockStyle.Left;
+            this.AboutusBtn.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaption;
+            this.AboutusBtn.FlatAppearance.BorderSize = 0;
+            this.AboutusBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AboutusBtn.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AboutusBtn.ForeColor = System.Drawing.Color.White;
+            this.AboutusBtn.Location = new System.Drawing.Point(0, 0);
+            this.AboutusBtn.Name = "AboutusBtn";
+            this.AboutusBtn.Size = new System.Drawing.Size(123, 67);
+            this.AboutusBtn.TabIndex = 1;
+            this.AboutusBtn.Text = "About Us";
+            this.AboutusBtn.UseVisualStyleBackColor = false;
+            this.AboutusBtn.Click += new System.EventHandler(this.AboutusBtn_Click);
+            // 
+            // ContactBtn
+            // 
+            this.ContactBtn.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ContactBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ContactBtn.Dock = System.Windows.Forms.DockStyle.Left;
+            this.ContactBtn.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ContactBtn.FlatAppearance.BorderSize = 0;
+            this.ContactBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ContactBtn.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ContactBtn.ForeColor = System.Drawing.Color.White;
+            this.ContactBtn.Location = new System.Drawing.Point(123, 0);
+            this.ContactBtn.Name = "ContactBtn";
+            this.ContactBtn.Size = new System.Drawing.Size(123, 67);
+            this.ContactBtn.TabIndex = 2;
+            this.ContactBtn.Text = "Contact";
+            this.ContactBtn.UseVisualStyleBackColor = false;
+            this.ContactBtn.Click += new System.EventHandler(this.ContactBtn_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.ServiceBtn);
+            this.panel1.Controls.Add(this.ContactBtn);
+            this.panel1.Controls.Add(this.AboutusBtn);
+            this.panel1.Location = new System.Drawing.Point(1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1245, 67);
+            this.panel1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label2.Location = new System.Drawing.Point(60, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 45);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Login";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(253, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 45);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Sign Up";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.panel2.Location = new System.Drawing.Point(59, 147);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(109, 4);
+            this.panel2.TabIndex = 7;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Transparent;
+            this.panel3.Controls.Add(this.panel4);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.panel2);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Location = new System.Drawing.Point(643, 70);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(603, 529);
+            this.panel3.TabIndex = 8;
+            // 
+            // panel4
+            // 
+            this.panel4.Location = new System.Drawing.Point(261, 147);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(109, 4);
+            this.panel4.TabIndex = 8;
             // 
             // Form1
             // 
@@ -37,16 +165,31 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1183, 620);
+            this.ClientSize = new System.Drawing.Size(1247, 664);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
             this.DoubleBuffered = true;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button ServiceBtn;
+        private System.Windows.Forms.Button AboutusBtn;
+        private System.Windows.Forms.Button ContactBtn;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,35 @@ namespace ArtisticLounge
         {
             InitializeComponent();
         }
+
+        private void AboutusBtn_Click(object sender, EventArgs e)
+        {
+            ServiceBtn.FlatStyle = FlatStyle.Flat;
+            ContactBtn.FlatStyle = FlatStyle.Flat;
+
+            AboutusBtn.FlatStyle = FlatStyle.System;
+        }
+
+        private void ServiceBtn_Click(object sender, EventArgs e)
+        {
+            ServiceBtn.FlatStyle = FlatStyle.System;
+
+            ContactBtn.FlatStyle = FlatStyle.Flat;
+            AboutusBtn.FlatStyle = FlatStyle.Flat;
+        }
+
+        private void ContactBtn_Click(object sender, EventArgs e)
+        {
+            ServiceBtn.FlatStyle = FlatStyle.Flat;
+
+            ContactBtn.FlatStyle = FlatStyle.System;
+
+            AboutusBtn.FlatStyle = FlatStyle.Flat;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
